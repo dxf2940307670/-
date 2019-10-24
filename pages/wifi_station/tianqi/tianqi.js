faf40674b94d44166cf89606f396fc6434963a6b
XJMeng <mengxiangji365@163.com>
2019-10-24 22:56:54 +0800

    添加注释

--- pages/wifi_station/tianqi/tianqi.js
+++ pages/wifi_station/tianqi/tianqi.js
@@ -57,6 +57,12 @@ Page({
     return new Promise((resolve, reject) => {
       wx.request({
         url: `https://api.heclouds.com/devices/${devicesId}/datapoints?datastream_id=Light,Temperature,Humidity&limit=20`,
+        /**
+         * 添加HTTP报文的请求头, 
+         * 其中api-key为OneNet的api文档要求我们添加的鉴权秘钥
+         * Content-Type的作用是标识请求体的格式, 从api文档中我们读到请求体是json格式的
+         * 故content-type属性应设置为application/json
+         */
         header: {
           'content-type': 'application/json',
           'api-key': api_key
@@ -64,13 +70,14 @@ Page({
         success: (res) => {
           const status = res.statusCode
           const response = res.data
-          if (status !== 200) {
+          if (status !== 200) { // 返回状态码不为200时将Promise置为reject状态
             reject(res.data)
           }
-          if (response.errno !== 0) {
+          if (response.errno !== 0) { //errno不为零说明可能参数有误, 将Promise置为reject
             reject(response.error)
           }
 
+          //程序可以运行到这里说明请求成功, 将Promise置为resolve状态
           resolve({
             temperature: response.data.datastreams[0].datapoints.reverse(),
             light: response.data.datastreams[1].datapoints.reverse(),
