24319f227fadaf3a3592bb42779ef37d795a1d85
XJMeng <mengxiangji365@163.com>
2019-10-27 12:12:25 +0800

    修改可能出现的报错信息

--- pages/wifi_station/tianqi/tianqi.js
+++ pages/wifi_station/tianqi/tianqi.js
@@ -16,7 +16,7 @@ Page({
     this.getDatapoints().then(datapoints => {
       this.update(datapoints)
       wx.hideLoading()
-    }).catch((err) => {
+    }).catch((error) => {
       wx.hideLoading()
       console.error(error)
     })
@@ -72,9 +72,16 @@ Page({
           const response = res.data
           if (status !== 200) { // 返回状态码不为200时将Promise置为reject状态
             reject(res.data)
+            return ;
           }
           if (response.errno !== 0) { //errno不为零说明可能参数有误, 将Promise置为reject
             reject(response.error)
+            return ;
+          }
+
+          if (response.data.datapoints.length === 0) {
+            reject(response.data)
+            return ;
           }
 
           //程序可以运行到这里说明请求成功, 将Promise置为resolve状态
